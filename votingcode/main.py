7343b8e8060bcbae4ce07c2e9a8c9edeca844d85
Sıtkı Ege Umay <sege.umay35@gmail.com>
2024-01-19 15:05:41 +0300

    Voting-Code
    I made voting code in Python. We can display the percentage of whether the vote is accepted or rejected.

---
+++ votingcode/main.py
@@ -0,0 +1,14 @@
+print("VOTE COUNT FOR YES")
+yes = int(input())
+print("VOTE COUNT FOR NO")
+no = int(input())
+result = yes / (yes + no)
+result_percentage = result * 100
+print("Result percentage: %", result_percentage)
+if result_percentage < 50:
+    print("REJECTED")
+elif result_percentage > 50:
+    print("ACCEPTED")
+else:
+    print("INVALID VOTE")
+2
